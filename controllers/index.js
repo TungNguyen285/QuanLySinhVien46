9b55dc16ad27e51c72ae1a8be0980aa35459440d
Nguyen Thanh Huyen <thanhhuyena61997@gmail.com>
2020-07-30 19:56:36 +0700

    them sinh vien

--- controllers/index.js
+++ controllers/index.js
@@ -1,16 +1,16 @@
 //Tạo dối tượng service tương tác backend
-var svService = new
-SinhVienService();
+var svService = new SinhVienService();
 
-var renderSinhVien = function(){
-    var promise = svService.layDanhSachSinhVien();
-    promise.then(function(res){
-        //Tạo <tr> table
-        //Tạo nội dung các thẻ tr
-        var contentTable = '';
-        for(var index = 0; index < res.data.length; index++){
-            var sinhVien = res.data[index];
-            contentTable += `
+var renderSinhVien = function () {
+  var promise = svService.layDanhSachSinhVien();
+  promise
+    .then(function (res) {
+      //Tạo <tr> table
+      //Tạo nội dung các thẻ tr
+      var contentTable = "";
+      for (var index = 0; index < res.data.length; index++) {
+        var sinhVien = res.data[index];
+        contentTable += `
                 <tr>
                     <td>${sinhVien.MaSV}</td>
                     <td>${sinhVien.HoTen}</td>
@@ -21,13 +21,21 @@ var renderSinhVien = function(){
                     <td>${sinhVien.DiemLy}</td>
                     <td>${sinhVien.DiemHoa}</td>
                 </tr>`;
-        }
-        //In ra giao diện
-        document.getElementById('tblSinhVien').innerHTML = contentTable;
-        //console.log(res.data)
-    }).catch(function(error){
-        console.log(error.response.data)
+      }
+      //In ra giao diện
+      document.getElementById("tblSinhVien").innerHTML = contentTable;
+      //console.log(res.data)
     })
-}
+    .catch(function (error) {
+      console.log(error.response.data);
+    });
+};
 
+var themSinhVien = function () {
+  console.log("thêm sinh viên");
+};
+//user B
+//git add -A
+//git commit -m "viết chức năng xóa sinh viên"
+//git push
 renderSinhVien();
